54156060370859f5323b43195855bf5a6746f4f6
Mani Shankar <manishankar@Manis-MacBook-Air.local>
2026-08-04 20:32:53 -0400

    Add web_sg security group; confirm httpd running and reachable on all instances

--- main.tf
+++ main.tf
@@ -24,6 +24,7 @@ resource "aws_instance" "machine" {
   count                = 3
   ami                  = data.aws_ami.amazon_linux.id
   instance_type        = "t3.micro"
+  vpc_security_group_ids = [aws_security_group.web_sg.id]
   iam_instance_profile = aws_iam_instance_profile.ec2_ssm_profile.name
   user_data_replace_on_change = true
   user_data = <<-EOF
@@ -41,5 +42,33 @@ resource "aws_instance" "machine" {
 
   lifecycle {
     ignore_changes = [ami]
+  }
+}
+
+resource "aws_security_group" "web_sg" {
+  name        = "web-sg"
+  description = "Allow HTTP and SSH"
+
+  ingress {
+    description = "HTTP"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "SSH"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
   }
 }
